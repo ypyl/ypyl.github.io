0f9abca3c22e0616c9a7be32590192d9553e2de7
Yauhen Pyl <yauhen.pyl@gmail.com>
2023-05-19 22:12:08 +0200

    current progress

--- Generators/MineSourceGenerator.cs
+++ Generators/MineSourceGenerator.cs
@@ -1,36 +1,55 @@
 ﻿using System.IO;
+using System.Text;
 using Markdig;
 using Markdown.ColorCode;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 
 namespace SourceGenerator
 {
     [Generator]
     public class MineSourceGenerator : ISourceGenerator
     {
-        private static readonly MarkdownPipeline MarkdownPipeline = new MarkdownPipelineBuilder()
-            .UseAdvancedExtensions()
-            .UseColorCode()
-            .Build();
-
         public void Execute(GeneratorExecutionContext context)
         {
+            var MarkdownPipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .UseColorCode()
+                .Build();
+            var code = new StringBuilder();
+            code.AppendLine(
+"""""""""
+using System.Collections.Generic;
+namespace Blog;
+public static class Articles
+{
+    public static IReadOnlyDictionary<string, string> Content()
+    {
+        return new Dictionary<string, string>
+        {
+""""""""");
             foreach (AdditionalText additionalFile in context.AdditionalFiles)
             {
                 // Process the additional file
                 var filePath = additionalFile.Path;
-                var fileName = Path.GetFileName(filePath);
+                var fileName = Path.GetFileNameWithoutExtension(filePath);
                 var fileContent = additionalFile.GetText(context.CancellationToken);
                 var html = Markdig.Markdown.ToHtml(fileContent.ToString(), MarkdownPipeline);
-                context.AddSource($"{fileName}.g.cs", $$"""
-                    namespace TestGenerator;
-                    public static class TestGeneratorNameMine
-                    {
-                        public static string BlogHtml() => "{{html}}";
-                    }
-                    """);
+                var content =
+$$"""""""""
+            ["""{{fileName}}"""] =
+"""""
+{{html}}
+""""",
+""""""""";
+                code.AppendLine(content);
             }
+            code.AppendLine(
+"""""""""
+        };
+    }
+}
+""""""""");
+            context.AddSource($"ArtcilesContent.g.cs", code.ToString());
         }
 
         public void Initialize(GeneratorInitializationContext context)
