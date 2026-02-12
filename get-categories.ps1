60d2afd51ad1cb61d74883352e5cc338688a0b03
Yauhen Pyl <yauhen.pyl@gmail.com>
2026-02-12 23:08:59 +0100

    check

--- get-categories.ps1
+++
@@ -1,7 +0,0 @@
-$categories = Get-ChildItem -Path "$PSScriptRoot\_tools" -Filter *.md | ForEach-Object {
-    if ((Get-Content $_.FullName -Raw) -match 'category:\s*(.+)') {
-        $matches[1].Trim()
-    }
-} | Select-Object -Unique
-
-$categories -join ', '
--- get-tags.ps1
+++
@@ -1,7 +0,0 @@
-$tags = Get-ChildItem -Path "$PSScriptRoot\_tools" -Filter *.md | ForEach-Object {
-    if ((Get-Content $_.FullName -Raw) -match 'tags:\s*\[([^\]]+)\]') {
-        $matches[1] -split ',\s*'
-    }
-} | Select-Object -Unique
-
-$tags -join ', '
